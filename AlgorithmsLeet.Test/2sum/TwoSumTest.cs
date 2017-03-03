4a11a5f64888d2b809730484e3a7a4339b761fef
xintao <xintao.zhang@tigerspike.com>
2017-03-03 16:59:40 +0800

    Initialize twosum to return index class

--- AlgorithmsLeet.Test/2sum/TwoSumTest.cs
+++ AlgorithmsLeet.Test/2sum/TwoSumTest.cs
@@ -7,6 +7,7 @@ namespace AlgorithmsLeet.Test._2sum
     [TestClass]
     public class TwoSumTest
     {
+        #region TwoSum
         [TestMethod]
         public void GivenArrayWithTwoSum_Should_GetTrue_Solution1()
         {
@@ -46,6 +47,33 @@ namespace AlgorithmsLeet.Test._2sum
             var result = twoSum.FindTwoSum_Solution2(input, target);
             Assert.AreEqual(false, result);
         }
+
+        #endregion
+
+
+        #region Two Sum to get index
+        [TestMethod]
+        public void GivenArrayWithTwoSum_Should_GetIndex()
+        {
+            var twoSumIndex = new TwoSum.TwoSumIndex();
+            int[] input = new int[] { 1, 5, 7, 3 };
+            var target = 10;
+            var result = twoSumIndex.TwoSumFindIndex(input, target);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(3, result[1]);
+        }
+
+        [TestMethod]
+        public void GivenArrayWithoutTwoSum_Should_Get_Null()
+        {
+            var twoSumIndex = new TwoSum.TwoSumIndex();
+            int[] input = new int[] { 1, 5, 7, 8 };
+            var target = 10;
+            var result = twoSumIndex.TwoSumFindIndex(input, target);
+            Assert.IsNull(result);
+        }
+        #endregion 
 
     }
 }
---
+++ AlgorithmsLeet/TwoSum/TwoSumIndex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.TwoSum
+{
+    public class TwoSumIndex
+    {
+        public int[] TwoSumFindIndex(int[] A, int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
