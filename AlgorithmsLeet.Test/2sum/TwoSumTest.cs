2ea9dc0410e7667507cf8ac6df145d0f89b86cc7
xintao <xintao.zhang@tigerspike.com>
2017-03-03 16:27:38 +0800

    2Sum implementation

---
+++ AlgorithmsLeet.Test/2sum/TwoSumTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsLeet.TwoSum;
+
+namespace AlgorithmsLeet.Test._2sum
+{
+    [TestClass]
+    public class TwoSumTest
+    {
+        [TestMethod]
+        public void GivenArrayWithTwoSum_Should_GetTrue_Solution1()
+        {
+            var twoSum = new TwoSum.TwoSum();
+            int[] input = new int[] { 1, 5, 7, 3 };
+            var target = 10;
+            var result = twoSum.FindTwoSum_Solution1(input, target);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void GivenArrayWithTwoSum_Should_GetTrue_Solution2()
+        {
+            var twoSum = new TwoSum.TwoSum();
+            int[] input = new int[] { 1, 5, 7, 3 };
+            var target = 10;
+            var result = twoSum.FindTwoSum_Solution2(input, target);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void GivenArrayWithoutTwoSum_Should_GetFalse_Solution1()
+        {
+            var twoSum = new TwoSum.TwoSum();
+            int[] input = new int[] { 1, 5, 7, 8};
+            var target = 10;
+            var result = twoSum.FindTwoSum_Solution1(input, target);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void GivenArrayWithoutTwoSum_Should_GetFalse_Solution2()
+        {
+            var twoSum = new TwoSum.TwoSum();
+            int[] input = new int[] { 1, 5, 7, 8 };
+            var target = 10;
+            var result = twoSum.FindTwoSum_Solution2(input, target);
+            Assert.AreEqual(false, result);
+        }
+
+    }
+}
---
+++ AlgorithmsLeet/TwoSum/TwoSum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.TwoSum
+{
+    public class TwoSum
+    {
+        public bool FindTwoSum_Solution1(int[] input, int target)
+        {
+            Array.Sort(input);
+
+            for (int i = 0, j = input.Length - 1; i < j;)
+            {
+                if (input[i] + input[j] > target)
+                {
+                    j--;
+                }
+                else if (input[i] + input[j] < target)
+                {
+                    i++;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool FindTwoSum_Solution2(int[] input, int target)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                dict.Add(input[i], i);
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (dict.ContainsKey(target - input[i]) && input[i] != (target-input[i]))
+                {
+                    return true;
+                }
+            }
+            return false; 
+        }
+    }
+}
