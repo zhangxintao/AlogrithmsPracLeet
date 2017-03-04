570f76dfc8d5e2f6e9467282c892c7356c5da71e
xintao <zhang_xintao@hotmail.com>
2017-03-04 21:44:25 +0800

    Move Array Related implementation to "Array" folder

---
+++ AlgorithmsLeet.Test/Array/MaxIndexDistance/MaxIndexDistanceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsLeet.Test.Array.MaxIndexDistance
+{
+    [TestClass]
+    public class MaxIndexDistanceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ AlgorithmsLeet/Array/MaxIndexDistance/MaxIndexDistance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Given an integer array A, find max value for i-j that satisfy A[i] < A[j] and i < j
+ */
+namespace AlgorithmsLeet.Array.MaxIndexDistance
+{
+    public class MaxIndexDistance
+    {
+        public int Get(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
