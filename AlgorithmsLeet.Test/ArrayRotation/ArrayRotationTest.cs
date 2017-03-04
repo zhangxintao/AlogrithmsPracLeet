96019d1f007290018ae6fadf0647da0466033657
xintao <zhang_xintao@hotmail.com>
2017-03-04 21:34:53 +0800

    Implemented  Array Rotation

---
+++ AlgorithmsLeet.Test/ArrayRotation/ArrayRotationTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsLeet.ArrayRotation;
+
+namespace AlgorithmsLeet.Test.ArrayRotation
+{
+    [TestClass]
+    public class ArrayRotationTest
+    {
+        [TestMethod]
+        public void Array_Rotate_Right_Should_Get_Rotated_Result()
+        {
+            var input = new int[] {1, 2, 3, 4, 5};
+            var rotation = new AlgorithmsLeet.ArrayRotation.ArrayRotation();
+            
+            var result = rotation.rotateRight(input, 2);
+
+            CollectionAssert.AreEqual(new int[]{4, 5, 1, 2, 3}, result);
+        }
+
+        [TestMethod]
+        public void Array_Rotate_0_Times_Should_Get_Original_Array()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5 };
+            var rotation = new AlgorithmsLeet.ArrayRotation.ArrayRotation();
+
+            var result = rotation.rotateRight(input, 0);
+
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, result);
+        }
+    }
+}
---
+++ AlgorithmsLeet/ArrayRotation/ArrayRotation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.ArrayRotation
+{
+    public class ArrayRotation
+    {
+        public int[] rotateRight(int[] input, int k)
+        {
+            if (k <= 0)
+            {
+                return input;
+            }
+
+            Reverse(input, 0, input.Length -1);
+            Reverse(input, 0, k-1);
+            Reverse(input, k, input.Length -1);
+
+            return input;
+        }
+
+        private void Reverse(int[] a, int start, int end)
+        {
+            for (int i = start, j = end; i < j; i++, j--)
+            {
+                var temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
