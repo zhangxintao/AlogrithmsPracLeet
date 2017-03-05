cb52f729b56cde1ff34819575cc782bd1c64789c
xintao <zhang_xintao@hotmail.com>
2017-03-05 20:12:35 +0800

    Implemented Digits Rearrange

---
+++ AlgorithmsLeet.Test/Arrays/DigitsRearrange/DigitsRearrangeTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsLeet.Test.Arrays.DigitsRearrange
+{
+    [TestClass]
+    public class DigitsRearrangeTest
+    {
+        [TestMethod]
+        public void GivenXandY_Rearrange_Return_Closet_Array_To_Y()
+        {
+            var x = new int[] {1, 3, 2, 4};
+            var y = new int[] {2, 4, 1, 0};
+            var expected = new int[] {2, 4, 1, 3};
+
+            var result = new AlgorithmsLeet.Arrays.DigitsRearrange.DigitsRearrange().Rearrange(x, y);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ AlgorithmsLeet/Arrays/DigitsRearrange/DigitsRearrange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.Arrays.DigitsRearrange
+{
+    public class DigitsRearrange
+    {
+        public int[] Rearrange(int[] x, int[] y)
+        {
+            Array.Sort(x);
+
+            var used = new bool[x.Length];
+            var result = new int[y.Length];
+
+            for (int i = 0; i < y.Length; i++)
+            {
+                int j = 0;
+                while (j < x.Length)
+                {
+                    if (x[j] >= y[i] && !used[j])
+                    {
+                        break;
+                    }
+                    j++;
+                }
+                result[i] = x[j];
+                used[j] = true;
+            }
+
+            return result;
+        }
+    }
+}
