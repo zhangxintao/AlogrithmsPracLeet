045ee2c3af7b8be283f7a5f564bf89d091ea182e
xintao <zhang_xintao@hotmail.com>
2017-03-05 17:45:18 +0800

    Implemented  Array Pair

---
+++ AlgorithmsLeet.Test/Arrays/ArrayPair/ArrayPairTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsLeet.Test.Arrays.ArrayPair
+{
+    [TestClass]
+    public class ArrayPairTest
+    {
+        [TestMethod]
+        public void GivenArrayWithValidPairs_Pair_Get_True()
+        {
+            var input = new int[] {2, 8, 4, 10, 1, 5};
+            var k = 6;
+
+            var result = new AlgorithmsLeet.Arrays.ArrayPair.ArrayPair().Pair(input, k);
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void GivenArrayWithoutValidPairs_Pair_Get_False()
+        {
+            var input = new int[] { 2, 8, 4, 7, 1, 5 };
+            var k = 6;
+
+            var result = new AlgorithmsLeet.Arrays.ArrayPair.ArrayPair().Pair(input, k);
+
+            Assert.AreEqual(false, result);
+        }
+    }
+}
---
+++ AlgorithmsLeet/Arrays/ArrayPair/ArrayPair.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.Arrays.ArrayPair
+{
+    public class ArrayPair
+    {
+        public bool Pair(int[] input, int k)
+        {
+            var reminders = new int[k];
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var reminder = input[i]%k;
+                reminders[reminder]++;
+            }
+
+            for (var i = 0; i < k; i++)
+            {
+                if (i == 0)
+                {
+                    if (reminders[i]%2 != 0) return false;
+                }
+                else if (k%2 == 0 && i == k/2)
+                {
+                    if (reminders[i]%2 != 0) return false;
+                }
+                else
+                {
+                    if ((reminders[i] + reminders[k - i])%2 != 0)
+                    {
+                        return false;
+                    }
+                }
+
+            }
+
+            return true;
+        }
+
+    }
+}
