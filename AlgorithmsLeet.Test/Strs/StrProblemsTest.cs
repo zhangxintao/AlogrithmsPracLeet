7548ef808efbb7116caf063e7a640c2c795b9c77
xintao <xintao.zhang@tigerspike.com>
2017-03-10 11:02:17 +0800

    Implemented check duplicate char in string

---
+++ AlgorithmsLeet.Test/Strs/StrProblemsTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsLeet.Strs;
+
+namespace AlgorithmsLeet.Test.Strs
+{
+    [TestClass]
+    public class StrProblemsTest
+    {
+        #region ContainsDuplication
+        [TestMethod]
+        public void GivenStringContainsDupliation_Return_True()
+        {
+            StrProblems strp = new StrProblems();
+            string str = "AbcadA";
+
+            var result = strp.ContainsDuplication(str);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GivenUniqueString_Return_False()
+        {
+            StrProblems strp = new StrProblems();
+            string str = "AbcadB";
+
+            var result = strp.ContainsDuplication(str);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ContainsDuplication_ASCII_GivenStringContainsDupliation_Return_True()
+        {
+            StrProblems strp = new StrProblems();
+            string str = "AbcadA";
+
+            var result = strp.ContainsDuplication_ASCII(str);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ContainsDuplication_ASCII_GivenUniqueString_Return_False()
+        {
+            StrProblems strp = new StrProblems();
+            string str = "AbcadB";
+
+            var result = strp.ContainsDuplication_ASCII(str);
+
+            Assert.IsFalse(result);
+        }
+        #endregion
+    }
+}
---
+++ AlgorithmsLeet/Strs/StrProblems.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.Strs
+{
+    public class StrProblems
+    {
+        public bool ContainsDuplication(string str)
+        {
+            Dictionary<char, int> strsDict = new Dictionary<char, int>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (strsDict.ContainsKey(str[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    strsDict.Add(str[i], 1);
+                }
+            }
+
+            return false;
+        }
+
+        public bool ContainsDuplication_ASCII(string str)
+        {
+            bool[] mark = new bool[256];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (mark[(int)str[i]])
+                {
+                    return true;
+                }
+                else
+                {
+                    mark[(int)str[i]] = true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
