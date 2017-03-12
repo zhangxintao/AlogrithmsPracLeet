92044190bea01efb5e75fcdedaa7baa39ed4c78d
xintao <zhang_xintao@hotmail.com>
2017-03-13 07:47:56 +0800

    Initialize wordbreak template

---
+++ AlgorithmsLeet.Test/DP/WordBreakTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmsLeet.DP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsLeet.Test.DP
+{
+    [TestClass]
+    public class WordBreakTest
+    {
+        [TestMethod]
+        public void GetSentenceRecur_Given_Not_Break_Sentence_And_Dict_Return_Valid_Sentence_Based_On_Dict()
+        {
+            WordBreak wb = new WordBreak();
+            var input = "thisisanexample";
+            var dict = new Dictionary<string, int>();
+            dict.Add("this", 1);
+            dict.Add("an", 1);
+            dict.Add("example", 1);
+            dict.Add("is", 1);
+
+            var result = wb.GetSentenceRecur(input, dict);
+
+            Assert.AreEqual(4, result.Count);
+            Assert.AreEqual("this", result[0]);
+            Assert.AreEqual("is", result[1]);
+            Assert.AreEqual("an", result[2]);
+            Assert.AreEqual("example", result[4]);
+        }
+    }
+}
---
+++ AlgorithmsLeet/DP/WordBreak.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.DP
+{
+    public class WordBreak
+    {
+        public List<string> GetSentenceRecur(string input, Dictionary<string, int> dict)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
