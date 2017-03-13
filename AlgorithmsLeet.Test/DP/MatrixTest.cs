78e99fbd33ad92ca7daa3cf877e942b6d6a907cb
xintao <xintao.zhang@tigerspike.com>
2017-03-13 17:45:35 +0800

    Initial checkin for Matrix minimum path sum

---
+++ AlgorithmsLeet.Test/DP/MatrixTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsLeet.DP;
+
+namespace AlgorithmsLeet.Test.DP
+{
+    [TestClass]
+    public class MatrixTest
+    {
+        [TestMethod]
+        public void MinimumSumOfPath_Given_Valid_Matrix_Return_Minimum_Sum_From_Top_To_Bottom()
+        {
+            var matrix = new Matrix();
+            var input 
+                = new int[][] { new[] { 2, 7, 3 },new[] { 1, 4, 5 },new[] { 10, 6, 9 } };
+
+            var result = matrix.MinimumSumOfPath(input);
+
+            Assert.AreEqual(21, result);
+        }
+    }
+}
