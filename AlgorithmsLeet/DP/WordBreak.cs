889de052462e4ce9b9194fa75047c1519c4c6be8
xintao <zhang_xintao@hotmail.com>
2017-03-13 08:09:43 +0800

    WordBreak halfway done - save work

--- AlgorithmsLeet/DP/WordBreak.cs
+++ AlgorithmsLeet/DP/WordBreak.cs
@@ -10,7 +10,37 @@ namespace AlgorithmsLeet.DP
     {
         public List<string> GetSentenceRecur(string input, Dictionary<string, int> dict)
         {
-            throw new NotImplementedException();
+            return GetSentenceRecur_Inner(input, dict, new Dictionary<string, List<string>>());
+        }
+
+        private List<string> GetSentenceRecur_Inner(string input, Dictionary<string, int> dict, Dictionary<string, List<string>> memoized )
+        {
+            var result = new List<string>();
+
+            if (String.IsNullOrEmpty(input))
+            {
+                return result;
+            }
+
+            if (memoized.ContainsKey(input))
+            {
+                return result;
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                var current = input.Substring(0, i + 1);
+                if (dict.ContainsKey(current))
+                {
+                    foreach (var suffix in GetSentenceRecur_Inner(input.Substring(i), dict, memoized))
+                    {
+                        result.Add(current + " " + suffix);   
+                    }
+                }
+                
+            }
+            memoized.Add(input, result);
+
+            return result;
         }
 
     }
