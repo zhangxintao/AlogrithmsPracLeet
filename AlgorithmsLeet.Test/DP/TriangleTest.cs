5cf366c92c9cd059c594593dfe06bae2bdef7541
xintao <zhang_xintao@hotmail.com>
2017-03-14 07:42:33 +0800

    Implemented Triangle Minimum Sum of Path

---
+++ AlgorithmsLeet.Test/DP/TriangleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AlgorithmsLeet.DP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsLeet.Test.DP
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void MinimumSumOfPath_Given_A_Triangle_Return_Minimum_Sum_Of_Top_Down_Path()
+        {
+            var triangle = new Triangle();
+            var input = new int[][] {new[] {2}, new[] {3, 4}, new[] {6, 5, 7}, new[] {8, 3, 9, 2}};
+
+            var result = triangle.MinimumSumOfPath(input);
+
+            Assert.AreEqual(13, result);
+        }
+    }
+}
---
+++ AlgorithmsLeet/DP/Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.DP
+{
+    public class Triangle
+    {
+        public int MinimumSumOfPath(int[][] input)
+        {
+            var dp = new int[input.Length, input[input.Length-1].Length];
+
+            for (int j = 0; j < input[input.Length - 1].Length; j++)
+            {
+                dp[input.Length - 1, j] = input[input.Length - 1][j];
+            }
+
+            for (int i = input.Length - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    dp[i, j] = input[i][j] +
+                               (dp[i + 1,j] > dp[i + 1,j + 1] ? dp[i + 1,j + 1] : dp[i + 1,j]);
+                }
+            }
+
+            return dp[0, 0];
+        }
+    }
+}
