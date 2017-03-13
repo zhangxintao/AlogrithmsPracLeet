1200d0dd3705f6c1e86038caa5d50507e1fb14d3
xintao <xintao.zhang@tigerspike.com>
2017-03-13 14:31:10 +0800

    Implemented WordBreak One

--- AlgorithmsLeet.Test/DP/WordBreakTest.cs
+++ AlgorithmsLeet.Test/DP/WordBreakTest.cs
@@ -21,11 +21,75 @@ namespace AlgorithmsLeet.Test.DP
 
             var result = wb.GetSentenceRecur(input, dict);
 
-            Assert.AreEqual(4, result.Count);
-            Assert.AreEqual("this", result[0]);
-            Assert.AreEqual("is", result[1]);
-            Assert.AreEqual("an", result[2]);
-            Assert.AreEqual("example", result[4]);
+            Assert.AreEqual(4, result.Split(' ').Length);
+            Assert.AreEqual("this", result.Split(' ')[0]);
+            Assert.AreEqual("is", result.Split(' ')[1]);
+            Assert.AreEqual("an", result.Split(' ')[2]);
+            Assert.AreEqual("example", result.Split(' ')[3]);
+        }
+
+        [TestMethod]
+        public void GetSentenceRecur_Given_Not_Break_Sentence_And_Overlap_Dict_Return_Valid_Sentence_Based_On_Dict()
+        {
+            WordBreak wb = new WordBreak();
+            var input = "thisisanexample";
+            var dict = new Dictionary<string, int>();
+            dict.Add("this", 1);
+            dict.Add("thi", 1);
+            dict.Add("an", 1);
+            dict.Add("example", 1);
+            dict.Add("exam", 1);
+            dict.Add("is", 1);
+
+            var result = wb.GetSentenceRecur(input, dict);
+
+            Assert.AreEqual(4, result.Split(' ').Length);
+            Assert.AreEqual("this", result.Split(' ')[0]);
+            Assert.AreEqual("is", result.Split(' ')[1]);
+            Assert.AreEqual("an", result.Split(' ')[2]);
+            Assert.AreEqual("example", result.Split(' ')[3]);
+        }
+
+        [TestMethod]
+        public void GetSentenceMemoi_Given_Not_Break_Sentence_And_Dict_Return_Valid_Sentence_Based_On_Dict()
+        {
+            WordBreak wb = new WordBreak();
+            var input = "thisisanexample";
+            var dict = new Dictionary<string, int>();
+            dict.Add("this", 1);
+            dict.Add("an", 1);
+            dict.Add("example", 1);
+            dict.Add("is", 1);
+
+            var result = wb.GetSentenceMemoi(input, dict);
+
+            Assert.AreEqual(4, result.Split(' ').Length);
+            Assert.AreEqual("this", result.Split(' ')[0]);
+            Assert.AreEqual("is", result.Split(' ')[1]);
+            Assert.AreEqual("an", result.Split(' ')[2]);
+            Assert.AreEqual("example", result.Split(' ')[3]);
+        }
+
+        [TestMethod]
+        public void GetSentenceMemoi_Given_Not_Break_Sentence_And_Overlap_Dict_Return_Valid_Sentence_Based_On_Dict()
+        {
+            WordBreak wb = new WordBreak();
+            var input = "thisisanexample";
+            var dict = new Dictionary<string, int>();
+            dict.Add("this", 1);
+            dict.Add("thi", 1);
+            dict.Add("an", 1);
+            dict.Add("example", 1);
+            dict.Add("exam", 1);
+            dict.Add("is", 1);
+
+            var result = wb.GetSentenceMemoi(input, dict);
+
+            Assert.AreEqual(4, result.Split(' ').Length);
+            Assert.AreEqual("this", result.Split(' ')[0]);
+            Assert.AreEqual("is", result.Split(' ')[1]);
+            Assert.AreEqual("an", result.Split(' ')[2]);
+            Assert.AreEqual("example", result.Split(' ')[3]);
         }
     }
 }
--- AlgorithmsLeet/DP/WordBreak.cs
+++ AlgorithmsLeet/DP/WordBreak.cs
@@ -8,38 +8,89 @@ namespace AlgorithmsLeet.DP
 {
     public class WordBreak
     {
-        public List<string> GetSentenceRecur(string input, Dictionary<string, int> dict)
+        public string GetSentenceRecur(string input, Dictionary<string, int> dict)
         {
-            return GetSentenceRecur_Inner(input, dict, new Dictionary<string, List<string>>());
+            return GetSentenceRecur_Inner(input, dict);
         }
 
-        private List<string> GetSentenceRecur_Inner(string input, Dictionary<string, int> dict, Dictionary<string, List<string>> memoized )
+        private string GetSentenceRecur_Inner(string input, Dictionary<string, int> dict)
         {
-            var result = new List<string>();
-
+            var result = string.Empty;
             if (String.IsNullOrEmpty(input))
             {
                 return result;
             }
 
-            if (memoized.ContainsKey(input))
-            {
-                return result;
-            }
+            
             for (int i = 0; i < input.Length; i++)
             {
                 var current = input.Substring(0, i + 1);
                 if (dict.ContainsKey(current))
                 {
-                    foreach (var suffix in GetSentenceRecur_Inner(input.Substring(i), dict, memoized))
+                    if (i + 1 == input.Length)
                     {
-                        result.Add(current + " " + suffix);   
+                        result += current;
+                    }
+                    else
+                    { 
+                        var suffix = GetSentenceRecur_Inner(input.Substring(i+1), dict);
+                        if (!string.IsNullOrEmpty(suffix))
+                        {
+                            result += current + " " + suffix;
+                        }
                     }
                 }
                 
             }
-            memoized.Add(input, result);
 
+            return result;
+        }
+
+
+        public string GetSentenceMemoi(string input, Dictionary<string, int> dict)
+        {
+            return GetSentenceMemoi_Inner(input, dict, new Dictionary<string, string>());
+        }
+
+        private string GetSentenceMemoi_Inner(string input, Dictionary<string, int> dict, Dictionary<string, string> memoi)
+        {
+            var result = string.Empty;
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return result;
+            }
+
+            if(memoi.ContainsKey(input))
+            {
+                return memoi[input];
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var current = input.Substring(0, i + 1);
+
+                if (dict.ContainsKey(current))
+                {
+                    if (i + 1 == input.Length)
+                    {
+                        result += current;
+                    }
+                    else
+                    {
+                        var suffix = GetSentenceMemoi_Inner(input.Substring(i + 1), dict, memoi);
+                        if (!string.IsNullOrEmpty(suffix))
+                        {
+                            result += current + " " + suffix;
+                        }
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                memoi.Add(input, result);
+            }
             return result;
         }
 
