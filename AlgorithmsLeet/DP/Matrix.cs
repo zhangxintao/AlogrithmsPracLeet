8ff7672d306d5ab9a1e1634dc018215a6645f42b
xintao <zhang_xintao@hotmail.com>
2017-03-13 23:14:34 +0800

    Implemented matrix  mimimun sum of path

--- AlgorithmsLeet/DP/Matrix.cs
+++ AlgorithmsLeet/DP/Matrix.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,28 @@ namespace AlgorithmsLeet.DP
     {
         public int MinimumSumOfPath(int[][] input)
         {
-            throw new NotImplementedException();
+            var dp = new int[input.Length, input[0].Length];
+            dp[0,0] = input[0][0];
+            for (int i = 1; i < input.Length; i++)
+            {
+                dp[0,i] = input[0][i] + input[0][i - 1];
+            }
+
+            for (int j = 1; j < input.Length; j++)
+            {
+                dp[j,0] = input[j][0] + input[j - 1][0];
+            }
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                for (int j = 1; j < input[0].Length; j++)
+                {
+                    var min = dp[i - 1, j] > dp[i, j - 1] ? dp[i, j - 1] : dp[i - 1, j];
+                    dp[i, j] = input[i][j] + min;
+                }
+            }
+
+            return dp[input.Length - 1, input[0].Length - 1];
         }
     }
 }
