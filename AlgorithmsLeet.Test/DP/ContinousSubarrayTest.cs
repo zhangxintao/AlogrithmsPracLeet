919d9ead7e0361b9f854173a8af2a655205b3928
xintao <xintao.zhang@tigerspike.com>
2017-03-13 16:52:06 +0800

    Implemented continuous subarrays max sum

---
+++ AlgorithmsLeet.Test/DP/ContinousSubarrayTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsLeet.DP;
+
+namespace AlgorithmsLeet.Test.DP
+{
+    [TestClass]
+    public class ContinousSubarrayTest
+    {
+        [TestMethod]
+        public void MaxSum_Given_Integer_Array_Return_Max_Sum_For_Continuous_Subarray()
+        {
+            var instance = new ContinousSubarray();
+            var input = new[] { -3, 1, -3, 4, -1, 2, 1 };
+
+            var maxSum = instance.MaxSum(input);
+
+            Assert.AreEqual(6, maxSum);
+        }
+    }
+}
---
+++ AlgorithmsLeet/DP/ContinousSubarray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.DP
+{
+    public class ContinousSubarray
+    {
+        public int MaxSum(int[] input)
+        {
+            var dp = new int[input.Length];
+            dp[0] = input[0];
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (dp[i - 1] <= 0)
+                {
+                    dp[i] = input[i];
+                }
+                else
+                {
+                    dp[i] = dp[i - 1] + input[i];
+                }
+            }
+
+            var max = 0;
+            for (int i = 0; i < dp.Length; i++)
+            {
+                if (dp[i] > max)
+                {
+                    max = dp[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
