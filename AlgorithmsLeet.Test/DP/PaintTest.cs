28eedd773177855911592289fd5e995568320e04
xintao <xintao.zhang@tigerspike.com>
2017-03-14 16:02:29 +0800

    Implemented Paint House

---
+++ AlgorithmsLeet.Test/DP/PaintTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsLeet.DP;
+
+namespace AlgorithmsLeet.Test.DP
+{
+    [TestClass]
+    public class PaintTest
+    {
+        [TestMethod]
+        public void MinimumCost_Given_Cost_Return_Minimum_Cost()
+        {
+            var paint = new Paint();
+            var cost = new int[][]
+            {
+                new int[] { 1, 2, 3},
+                new int[] { 2, 2, 3},
+                new int[] { 4, 2, 3},
+                new int[] { 2, 1, 3}
+            };
+
+            var result = paint.MinimumCost(cost);
+
+            Assert.AreEqual(7, result);
+        }
+    }
+}
---
+++ AlgorithmsLeet/DP/Paint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.DP
+{
+    public class Paint
+    {
+        public int MinimumCost(int[][] cost)
+        {
+            var dp = new int[cost.Length, cost[0].Length];
+
+            dp[0, 0] = cost[0][0];
+            dp[0, 1] = cost[0][1];
+            dp[0, 2] = cost[0][2];
+
+            for (int i = 1; i < cost.Length; i++)
+            {
+                dp[i, 0] = Math.Min(dp[i - 1, 1], dp[i - 1, 2]) + cost[i][0];
+                dp[i, 1] = Math.Min(dp[i - 1, 0], dp[i - 1, 2]) + cost[i][1];
+                dp[i, 2] = Math.Min(dp[i - 1, 0], dp[i-1, 1]) + cost[i][2]; 
+            }
+
+            return Math.Min(dp[dp.GetLength(0) - 1, 0], Math.Min(dp[dp.GetLength(0) - 1, 1], dp[dp.GetLength(0) - 1, 2]));
+        }
+    }
+}
