5b65b304cda060fb3d3305d7aa642363ec0c4a5e
xintao <xintao.zhang@tigerspike.com>
2017-03-14 17:30:37 +0800

    Implemented Decode

---
+++ AlgorithmsLeet.Test/DP/DecodeTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsLeet.DP;
+
+namespace AlgorithmsLeet.Test.DP
+{
+    [TestClass]
+    public class DecodeTest
+    {
+        [TestMethod]
+        public void Execute_Given_Encoded_Numbers_String_Return_Ways_of_Decoding()
+        {
+            var decode = new Decode();
+            var input1 = "1";
+            var input2 = "12";
+            var input3 = "12926";
+            var input4 = "10";
+
+            var result1 = decode.Execute(input1);
+            var result2 = decode.Execute(input2);
+            var result3 = decode.Execute(input3);
+            var result4 = decode.Execute(input4);
+
+            Assert.AreEqual(1, result1);
+            Assert.AreEqual(2, result2);
+            Assert.AreEqual(4, result3);
+            Assert.AreEqual(1, result4);
+        }
+    }
+}
---
+++ AlgorithmsLeet/DP/Decode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.DP
+{
+    public class Decode
+    {
+        public int Execute(string encoded)
+        {
+            if (String.IsNullOrEmpty(encoded))
+            {
+                return 0;
+            }
+            var dp = new int[encoded.Length];
+
+            if (encoded[0] >= '1' && encoded[0] <= '9')
+            {
+                dp[0] = 1;
+            }
+            else
+            {
+                dp[0] = 0;
+            }
+
+            for (int i = 1; i < encoded.Length; i++)
+            {
+                if (encoded[i] == '0')
+                {
+                    dp[i] = dp[i - 1];
+                }
+                else
+                {
+                    if (encoded[i - 1] >= '2' || (encoded[i - 1] == '2' && encoded[i] > '6') || encoded[i] == '0')
+                    {
+                        dp[i] = dp[i - 1];
+                    }
+
+                    if ((encoded[i - 1] == '1' && encoded[i] != '0') || (encoded[i - 1] == '2' && encoded[i] <= '6'))
+                    {
+                        dp[i] = dp[i - 1] * 2;
+                    }
+                }
+               
+            }
+
+            return dp[dp.Length - 1];
+        }
+    }
+}
