ff2e24356b089c906414fc642c1e9e2b20fb9f83
xintao <zhang_xintao@hotmail.com>
2017-03-14 08:01:52 +0800

    Recursive implementation for stairs climbing

---
+++ AlgorithmsLeet.Test/DP/StairTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AlgorithmsLeet.DP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsLeet.Test.DP
+{
+    [TestClass]
+    public class StairTest
+    {
+        [TestMethod]
+        public void GetWaysOfClimb_Given_Stairs_Return_Ways_of_Climbing()
+        {
+            var stair = new Stair();
+
+            var result = stair.GetWaysOfClimb(4);
+
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ AlgorithmsLeet/DP/Stair.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.DP
+{
+    public class Stair
+    {
+        public int GetWaysOfClimb(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            if (n == 2)
+            {
+                return 2;
+            }
+
+            return GetWaysOfClimb(n - 1) + GetWaysOfClimb(n - 2);
+
+        }
+
+        public int GetWaysOfClimbDP(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
