71c03fe831ec5fdd8133ffc1d2c7ddf08a02479b
xintao <zhang_xintao@hotmail.com>
2017-03-14 22:19:46 +0800

    Implemented Longest Increasing Sequence Count

---
+++ AlgorithmsLeet.Test/DP/LongestInsreasingSubsequenceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AlgorithmsLeet.DP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsLeet.Test.DP
+{
+    [TestClass]
+    public class LongestInsreasingSubsequenceTest
+    {
+        [TestMethod]
+        public void MaxCount_Given_Integer_Sequence_Return_Longest_Increasing_Subsequence_Count()
+        {
+            var lis = new LongestIncreasingSubsequence();
+            var input = new int[] {-3, 1, 7, 5, 6, 8};
+
+            var count = lis.MaxCount(input);
+
+            Assert.AreEqual(5, count);
+        }
+    }
+}
---
+++ AlgorithmsLeet/DP/LongestIncreasingSubsequence.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.DP
+{
+    public class LongestIncreasingSubsequence
+    {
+        public int MaxCount(int[] input)
+        {
+            var dp = new int[input.Length];
+            dp[0] = 1;
+            int max = 1;
+            for (int i = 1; i < input.Length; i++)
+            {
+                var currentLongest = 0;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (input[j] < input[i])
+                    {
+                        currentLongest = Math.Max(dp[j], currentLongest);
+                    }
+                }
+                dp[i] = currentLongest + 1;
+                max = Math.Max(dp[i], max);
+            }
+
+            return max;
+        }
+    }
+}
