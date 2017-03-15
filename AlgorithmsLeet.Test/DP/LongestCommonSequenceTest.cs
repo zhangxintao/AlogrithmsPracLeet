7bb176c4c51ff1621be7df78de8d2827463c187b
xintao <zhang_xintao@hotmail.com>
2017-03-15 22:13:00 +0800

    Implemented LCS

---
+++ AlgorithmsLeet.Test/DP/LongestCommonSequenceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AlgorithmsLeet.DP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsLeet.Test.DP
+{
+    [TestClass]
+    public class LongestCommonSequenceTest
+    {
+        [TestMethod]
+        public void GetLongestSubsequence_Given_Two_String_With_Common_Sequence_Return_The_Longest_Common_Sequence()
+        {
+            var lcs = new LongestCommonSequence();
+            var s1 = "ABCD";
+            var s2 = "EDCA";
+
+            var result = lcs.GetLongestSubsequence(s1, s2);
+
+            Assert.AreEqual("A", result);
+        }
+    }
+}
---
+++ AlgorithmsLeet/DP/LongestCommonSequence.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.DP
+{
+    public class LongestCommonSequence
+    {
+        public string GetLongestSubsequence(string s1, string s2)
+        {
+            var res = "";
+            var m = s1.Length;
+            var n = s2.Length;
+
+            var dp = new int[m+1, n+1];
+            int max = 0;
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        dp[i, j] = 0;
+                    }
+                    else
+                    {
+                        if (s1[i-1] == s2[j-1])
+                        {
+                            dp[i, j] = dp[i - 1, j - 1] + 1;
+                        }
+                        else
+                        {
+                            dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                        }   
+                    }
+
+                    if (dp[i, j] > max)
+                    {
+                        res += s2[j-1];
+                        max = dp[i, j];
+                    }
+
+                }
+            }
+            return res;
+        }
+    }
+}
