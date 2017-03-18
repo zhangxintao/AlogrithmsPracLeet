d00f83c7728614af74dc897eba7924de3aaaad41
xintao <zhang_xintao@hotmail.com>
2017-03-18 16:19:22 +0800

    Implemented Merge sorted arrays

---
+++ AlgorithmsLeet.Test/Arrays/MergeSortedArrayTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using AlgorithmsLeet.Arrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsLeet.Test.Arrays
+{
+    [TestClass]
+    public class MergeSortedArrayTest
+    {
+        [TestMethod]
+        public void Merge_Given_Array_A_B_Return_Merge_B_Into_A()
+        {
+            var a = new int[10] {2, 4, 6, 8, 10, 12, 0, 0, 0, 0};
+            var b = new int[] {1, 3, 5, 7};
+            var merge = new MergeSortedArray();
+
+            merge.Merge(a, b);
+
+            CollectionAssert.AreEqual(new int[]{1, 2, 3, 4,5, 6, 7, 8, 10, 12}, a);
+        }
+    }
+}
---
+++ AlgorithmsLeet/Arrays/MergeSortedArray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.Arrays
+{
+    public class MergeSortedArray
+    {
+        public void Merge(int[] a, int[] b)
+        {
+            int indexa = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == 0)
+                {
+                    indexa = i - 1;
+                    break;
+                }
+            }
+            var indexb = b.Length - 1;
+
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                if (indexa < 0)
+                {
+                    a[i] = b[indexb];
+                    indexb--;
+                }
+                else if (indexb < 0)
+                {
+                    a[i] = a[indexa];
+                    indexa--;
+                }
+                else
+                {
+                    if (a[indexa] >= b[indexb])
+                    {
+                        a[i] = a[indexa];
+                        indexa--;
+                    }
+                    else
+                    {
+                        a[i] = b[indexb];
+                        indexb--;
+                    }   
+                }
+                
+            }
+        }
+    }
+}
