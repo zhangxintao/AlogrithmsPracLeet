a5a0f5ad1548d6a551029fcd0bce0288751bf041
xintao <zhang_xintao@hotmail.com>
2017-03-22 07:39:00 +0800

    Implemented remove duplicates from array

---
+++ AlgorithmsLeet.Test/TwoPointers/TwoPointersTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsLeet.Test.TwoPointers
+{
+    [TestClass]
+    public class TwoPointersTest
+    {
+        [TestMethod]
+        public void RemoveDuplicatesFromOrderedArray()
+        {
+            var a = new int[] {1, 1, 2, 3, 3};
+            var instance = new AlgorithmsLeet.TwoPointers.TwoPointers();
+
+            var result = instance.RemoveDuplicatesFromOrderedArray(a);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ AlgorithmsLeet/TwoPointers/TwoPointers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLeet.TwoPointers
+{
+    public class TwoPointers
+    {
+        #region remove duplicates from ordered array
+
+        public int RemoveDuplicatesFromOrderedArray(int[] a)
+        {
+            if (a == null || a.Length == 0)
+            {
+                return 0;
+            }
+
+            if (a.Length == 1)
+            {
+                return 1;
+            }
+            int j = 0;
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] != a[j])
+                {
+                    j++;
+                    a[j] = a[i];
+                }
+            }
+
+            return j + 1;
+        }
+
+        #endregion
+    }
+}
