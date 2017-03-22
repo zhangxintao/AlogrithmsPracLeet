957050f3577d40ec4ffaf28ca8d1c534c336fa32
xintao <zhang_xintao@hotmail.com>
2017-03-22 22:39:24 +0800

    Implemented three sum and max stock profit

--- AlgorithmsLeet.Test/TwoPointers/TwoPointersTest.cs
+++ AlgorithmsLeet.Test/TwoPointers/TwoPointersTest.cs
@@ -16,5 +16,30 @@ namespace AlgorithmsLeet.Test.TwoPointers
 
             Assert.AreEqual(3, result);
         }
+
+        [TestMethod]
+        public void ThreeSum_Given_Array_With_ThreeSum_Return_Any_Of_Them()
+        {
+            var a = new int[] {-2, 1, -1, 2, 1};
+            var instance = new AlgorithmsLeet.TwoPointers.TwoPointers();
+
+            var result = instance.ThreeSum(a);
+
+            Assert.AreEqual(3, result.Length);
+            Assert.AreEqual(-2, result[0]);
+            Assert.AreEqual(1, result[1]);
+            Assert.AreEqual(1, result[2]);
+        }
+
+        [TestMethod]
+        public void MaxStockProfit_Given_StockPrices_Return_Max_Profit()
+        {
+            var a = new[] {7, 5, 1, 3, 6, 4};
+            var instance = new AlgorithmsLeet.TwoPointers.TwoPointers();
+
+            var result = instance.MaxStockProfit(a);
+
+            Assert.AreEqual(5, result);
+        }
     }
 }
--- AlgorithmsLeet/TwoPointers/TwoPointers.cs
+++ AlgorithmsLeet/TwoPointers/TwoPointers.cs
@@ -35,5 +35,68 @@ namespace AlgorithmsLeet.TwoPointers
         }
 
         #endregion
+
+        #region Three Sum
+
+        public int[] ThreeSum(int[] a)
+        {
+            if (a.Length < 3) return a;
+
+            Array.Sort(a);
+
+            for (int i = 0; i < a.Length-2;i++)
+            {
+                int head = i + 1;
+                int end = a.Length - 1;
+                while (head < end)
+                {
+                    if (a[head] + a[end] + a[i] == 0)
+                    {
+                        return new int[] {a[i], a[head], a[end]};
+                    }
+                    else if (a[i] + a[head] + a[end] > 0)
+                    {
+                        end = end - 1;
+                    }
+                    else
+                    {
+                        head = head + 1;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region Stock Transaction
+
+        public int MaxStockProfit(int[] a)
+        {
+            if (a == null || a.Length <= 1)
+            {
+                return 0;
+            }
+
+            int max = 0;
+            int minI = 0;
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] > a[minI])
+                {
+                    max = Math.Max(a[i] - a[minI], max);
+                }
+                else
+                {
+                    minI = i;
+                }
+            }
+
+            return max;
+        }
+
+        #endregion
     }
 }
